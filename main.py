668a8d4f1e18c587446c9994a90f39b7c2145e62
FilipKDev <filip.kwasniewski@gmail.com>
2025-01-22 13:26:49 +0000

    added feature that counts number of each character in the book

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
@@ -8,7 +10,24 @@ def count_words(book_content):
     words = book_content.split()
     for word in words:
         word_count += 1
-    print(word_count)
+    # print(word_count)
+
+def count_characters(book_content):
+    book_content_lowered = book_content.lower()
+    character_count = {}
+    characters = list(string.ascii_lowercase)
+    characters.append(" ")
+
+    for character in characters:
+        character_count[character] = 0
+
+    for book_character in book_content_lowered:
+        for character in characters:
+            if character == book_character:
+                character_count[character] += 1
+    print(character_count)
+    return character_count
 
 book_content = main()
 count_words(book_content)
+count_characters(book_content)
